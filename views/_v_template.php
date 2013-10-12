3316c0401fa034ac63ad64326b51454f87c778ba
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-11 20:46:37 -0400

    Tabs to spaces

--- views/_v_template.php
+++ views/_v_template.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php if(isset($title)) echo $title; ?></title>
+    <title><?php if(isset($title)) echo $title; ?></title>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-					
-	<!-- Controller Specific JS/CSS -->
-	<?php if(isset($client_files_head)) echo $client_files_head; ?>
-	
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />   
+                    
+    <!-- Controller Specific JS/CSS -->
+    <?php if(isset($client_files_head)) echo $client_files_head; ?>
+    
 </head>
 
-<body>	
+<body>  
 
-	<?php if(isset($content)) echo $content; ?>
+    <?php if(isset($content)) echo $content; ?>
 
-	<?php if(isset($client_files_body)) echo $client_files_body; ?>
+    <?php if(isset($client_files_body)) echo $client_files_body; ?>
 </body>
 </html>
