0cc1c06efec66b97d19be97cc1affe60f058dbb9
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-11 20:47:18 -0400

    Example using _v_template

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,1 +1,11 @@
-<h1>This is the profile for <?=$user_name?></h1>
+<!-- 
+	OK to put some display logic in view 
+	Also using alternative syntax - this should be a mostly HTML file
+-->
+<?php if (isset($user_name)): ?>
+	<h1>This is the profile for <?=$user_name?></h1>
+<?php else: ?>
+	<h1>No user has been specified</h1>
+<?php endif; ?>
+
+Color is <?=$color?><br />
