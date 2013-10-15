7d1d535bc8d04d4f0da9921b52d678e3c82478f4
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-14 21:28:19 -0400

    Centered buttons in navbar

--- views/_v_navbar.php
+++ views/_v_navbar.php
@@ -3,11 +3,13 @@
     <div class="navbar navbar-fixed-top">
         <div class="navbar-inner">
         <div class="container">
+            <div class="row offset2 span6">
             <a class="brand" href="#">Flitter</a>
             <ul class="nav">
                 <li class="active"><a href="#">Stream</a></li>
                 <li><a href="#">Find</a></li>
             </ul>
+            </div>
         </div>
         </div>
     </div>
