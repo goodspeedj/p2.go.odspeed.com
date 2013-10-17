44cfaa0f6e7d6cd49c6e2262be4769c592180b6e
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-16 20:36:03 -0400

    Reduced tab size from 4 to 2

--- views/_v_navbar.php
+++ views/_v_navbar.php
@@ -1,17 +1,17 @@
 <!-- Generate the Navigation bar -->
 <div class="container">
-    <div class="navbar navbar-fixed-top">
-        <div class="navbar-inner">
-        <div class="container">
-            <div class="row offset2 span6">
-            <a class="brand" href="#">Flitter</a>
-            <ul class="nav">
-                <li class="active"><a href="#">Stream</a></li>
-                <li><a href="#">Find</a></li>
-            </ul>
-            </div>
-        </div>
-        </div>
+  <div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+    <div class="container">
+      <div class="row offset2 span6">
+      <a class="brand" href="#">Flitter</a>
+      <ul class="nav">
+        <li class="active"><a href="#">Post</a></li>
+        <li><a href="#">Find</a></li>
+        <li><a href="/users/profile/">Profile</a></li>
+      </ul>
+      </div>
     </div>
+    </div>
+  </div>
 </div>
-
--- views/_v_template.php
+++ views/_v_template.php
@@ -1,44 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title><?php if(isset($title)) echo $title; ?></title>
+  <title><?php if(isset($title)) echo $title; ?></title>
 
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="/css/bootstrap-responsive.min.css"> 
-    <link rel="stylesheet" type="text/css" href="/css/p2-main.css">               
+  <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="/css/bootstrap-responsive.min.css"> 
+  <link rel="stylesheet" type="text/css" href="/css/p2-main.css">               
 
-    <!-- Controller Specific JS/CSS -->
-    <?php if(isset($client_files_head)) echo $client_files_head; ?>
-    
+  <!-- Controller Specific JS/CSS -->
+  <?php if(isset($client_files_head)) echo $client_files_head; ?>
+  
 </head>
 
 <body>  
 
-    <!-- 
-        Show or hide the nav bar.  Based on example from Piazza post #339
-        https://piazza.com/class/hktc23zr2apnf?cid=339
-    -->
-    <?php if(!$hide_navbar): ?>
-        <?=$navbar;?>
-    <?php endif; ?>
+  <!-- 
+    Show or hide the nav bar.  Based on example from Piazza post #339
+    https://piazza.com/class/hktc23zr2apnf?cid=339
+  -->
+  <?php if(!$hide_navbar): ?>
+    <?=$navbar;?>
+  <?php endif; ?>
 
-    <!-- Display main content -->
-    <div class="container">
-        <div class="row">
-            <div class="span3"></div>
-            <div class="span6">
-                <?php if(isset($content)) echo $content; ?>
-                <?php if(isset($client_files_body)) echo $client_files_body; ?>
-            </div>
-            <div class="span3"></div>
-        </div>
-        
+  <!-- Display main content -->
+  <div class="container">
+    <div class="row">
+      <div class="span3"></div>
+      <div class="span6">
+        <?php if(isset($content)) echo $content; ?>
+        <?php if(isset($client_files_body)) echo $client_files_body; ?>
+      </div>
+      <div class="span3"></div>
     </div>
     
-    <script src="/js/jquery-2.0.3.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
+  </div>
+  
+  <script src="/js/jquery-2.0.3.min.js"></script>
+  <script src="/js/bootstrap.min.js"></script>
 </body>
 </html>
