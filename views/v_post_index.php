e1f3bbfdda092b83675b65895225c01b9cab23f4
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-16 20:43:39 -0400

    Place holder added for posts

--- views/v_post_index.php
+++ views/v_post_index.php
@@ -1,6 +1,6 @@
 <table width="100%">
   <tr>
-    <td>
+    <td colspan="2">
       <textarea class="form-control new_post" rows="3" cols="100" autofocus form="post">
         Enter a new post...
       </textarea>
@@ -8,6 +8,11 @@
   </tr>
   <tr>
     <td>
+      <img src="http://lorempixel.com/32/32/abstract/">
+    </td>
+    <td>
+      <span class="bold">User Name:</span><br />
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit...
     </td>
   </tr>
 </table>
