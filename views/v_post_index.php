451be0237e8c6985d135b5127ac9b2b830f7b886
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-16 20:36:49 -0400

    Post index view - added new post textarea box

---
+++ views/v_post_index.php
@@ -0,0 +1,13 @@
+<table width="100%">
+  <tr>
+    <td>
+      <textarea class="form-control new_post" rows="3" cols="100" autofocus form="post">
+        Enter a new post...
+      </textarea>
+    </td>
+  </tr>
+  <tr>
+    <td>
+    </td>
+  </tr>
+</table>
