cbf6e8972e3fe8ce6a74aa61c308eadf8d9fb2d0
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-17 20:38:10 -0400

    Changed indentation to 2 from 4

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,19 +1,19 @@
 <!-- 
-	OK to put some display logic in view 
-	Also using alternative syntax - this should be a mostly HTML file
+  OK to put some display logic in view 
+  Also using alternative syntax - this should be a mostly HTML file
 -->
 <?php if (isset($user_name)): ?>
-	<h3>Profile for <?=$user_name?></h3>
-    <table>
-        <tr>
-            <td>Name:</td>
-            <td>value</td>
-        </tr>
-        <tr>
-            <td>Email:</td>
-            <td>value</td>
-        </tr>
-    </table>
+  <h3>Profile for <?=$user_name?></h3>
+  <table>
+    <tr>
+      <td>Name:</td>
+      <td>value</td>
+    </tr>
+    <tr>
+      <td>Email:</td>
+      <td>value</td>
+    </tr>
+  </table>
 <?php else: ?>
-	<h3>No user has been specified</h3>
+  <h3>No user has been specified</h3>
 <?php endif; ?>
