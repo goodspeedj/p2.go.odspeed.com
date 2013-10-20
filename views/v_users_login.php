046f43512935fab3f5e63f4825a7aaedd144da6e
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-19 20:16:06 -0400

    Added actions to tie back into users controller

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,17 +1,17 @@
 <div class="container">
 
-  <form class="form-signin">
+  <form class="form-signin" method="post" action="/users/p_login">
     <h3 class="form-signin-heading">Please sign in</h3>
-    <input type="text" placeholder="Email address">
+    <input type="text" name="email" placeholder="Email address">
     <br />
-    <input type="password" placeholder="Password">
+    <input type="password" name="password" placeholder="Password">
     <!--
     <label class="checkbox">
       <input type="checkbox" value="remember-me"> Remember me
     </label>
     -->
     <p>&nbsp;</p>
-    <button class="btn btn-primary" type="submit">Sign in</button>
+    <button class="btn btn-primary" type="submit">Log in</button>
   </form>
 
 </div>
