22014fb197520ae4cb5aeb5393f8fe6978a36cf3
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-20 20:59:48 -0400

    Fixed indentation

--- views/_v_navbar.php
+++ views/_v_navbar.php
@@ -8,24 +8,24 @@
       </button>
       
       <div class="offset1">
-      <a class="brand" href="/users/login">Flitter</a>
-      
-      <div class="nav-collapse collapse offset1">
-        <ul class="nav">
-          <li class="active">
-            <a href="./index.html">Home</a>
-          </li>
-          <li>
-            <a href="/post/index">Post</a>
-          </li>
-          <li>
-            <a href="/post/find">Find</a>
-          </li>
-          <li>
-            <a href="/users/profile">Profile</a>
-          </li>
-        </ul>
-      </div>
+        <a class="brand" href="/users/login">Flitter</a>
+        
+        <div class="nav-collapse collapse offset1">
+          <ul class="nav">
+            <li class="active">
+              <a href="./index.html">Home</a>
+            </li>
+            <li>
+              <a href="/post/index">Post</a>
+            </li>
+            <li>
+              <a href="/post/find">Find</a>
+            </li>
+            <li>
+              <a href="/users/profile">Profile</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
    </div>
