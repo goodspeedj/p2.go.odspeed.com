fbecec4afcb3d7b209c51cb01cc46e54e96299ab
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-22 20:50:24 -0400

    User list view

---
+++ views/v_users_index.php
@@ -0,0 +1,15 @@
+<!-- Displays a list of users -->
+<table class="user_list">
+  <tr>
+    <td>
+      <img src="http://lorempixel.com/32/32/abstract/">
+    </td>
+    <td>
+      <span class="bold">User Name:</span> Joe blow<br />
+      <span class="bold">Email:</span> Joe@blah.com
+    </td>
+    <td>
+      <a class="btn btn-small btn-success" href="#"><i class="icon-ok icon-white"></i>  Follow</a>
+    </td>
+  </tr>
+</table>
