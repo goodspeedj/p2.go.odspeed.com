1024a791ae75f702a6b7f8554d7fcbac87c24615
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-23 21:10:08 -0400

    User list page - controller and view basic functionality done

--- views/v_users_index.php
+++ views/v_users_index.php
@@ -1,15 +1,22 @@
 <!-- Displays a list of users -->
 <table class="user_list">
-  <tr>
-    <td>
-      <img src="http://lorempixel.com/32/32/abstract/">
-    </td>
-    <td>
-      <span class="bold">User Name:</span> Joe blow<br />
-      <span class="bold">Email:</span> Joe@blah.com
-    </td>
-    <td>
-      <a class="btn btn-small btn-success" href="#"><i class="icon-ok icon-white"></i>  Follow</a>
-    </td>
-  </tr>
+
+<?php foreach ($user_list as $row): ?>
+    <tr>
+      <td>
+        <img src="http://lorempixel.com/32/32/abstract/">
+      </td>
+      <td>
+        <span class="bold">User Name: </span>
+        <?php echo $row['first_name'] . " " . $row['last_name'] ?>
+        <br />
+        <span class="bold">Email: </span>
+        <?php echo $row['email'] ?>
+      </td>
+      <td>
+        <a class="btn btn-small btn-success" href="#"><i class="icon-ok icon-white"></i>  Follow</a>
+      </td>
+    </tr>
+<?php endforeach; ?>
+
 </table>
