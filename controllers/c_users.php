d3dd0030d38e825b477dbfea8cd33bb68617e1c2
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-24 20:33:02 -0400

    Implemented the logout method

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -120,11 +120,15 @@ class users_controller extends base_controller {
      */
     public function logout() {
 
-        // remove cookie
-        if(isset($_COOKIE)) {
-            unset($_COOKIE['token']);
-            setcookie('token', '', time() - 3600);
-        }
+        // Create a new dummy token 
+        $new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
+
+        $data = Array('token' => $new_token);
+
+        DB::instance(DB_NAME)->update('users', $data, 'WHERE user_id ='. $this->user->user_id);
+
+
+        setcookie('token', '', strtotime('-1 year'), '/');
         
 
         // Send user back to login page
