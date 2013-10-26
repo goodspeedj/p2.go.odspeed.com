9ced949ed63d255883b4782e795eedf497ea4216
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-25 21:39:47 -0400

    Added follow and unfollow methods, removed unused methods and udated posts query to only return followed posts

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -17,12 +17,18 @@ class posts_controller extends base_controller {
 
         // Get the posts
         $sql = 'SELECT 
-                    posts.*,
+                    posts.content,
+                    posts.created,
+                    posts.user_id AS post_user_id,                       
+                    users_users.user_id AS follower_id,
                     users.first_name,
                     users.last_name
                 FROM posts
-                INNER JOIN users
-                ON posts.user_id = users.user_id';
+                INNER JOIN users_users                         
+                    ON posts.user_id = users_users.user_id_followed
+                INNER JOIN users 
+                    ON posts.user_id = users.user_id
+                WHERE users_users.user_id = '.$this->user->user_id;
 
         $posts = DB::instance(DB_NAME)->select_rows($sql);
 
@@ -71,20 +77,36 @@ class posts_controller extends base_controller {
     }
 
 
-    public function edit() {
-        echo "This is the signup page";
+
+    public function follow($user_id_followed) {
+        
+        # Prepare the data array to be inserted
+        $data = Array(
+            "created"          => Time::now(),
+            "user_id"          => $this->user->user_id,
+            "user_id_followed" => $user_id_followed
+            );
+        
+        # Do the insert
+        DB::instance(DB_NAME)->insert('users_users', $data);
+        
+        # Send them back
+        Router::redirect("/posts/users");
+        
     }
 
-    public function like() {
-        echo "This is the signup page";
-    }
 
-    public function follow() {
-        echo "This is the signup page";
-    }
-
-    public function viewStream() {
-        echo "This is the signup page";
-    }
+    public function unfollow($user_id_followed) {
+        
+        # Set up the where condition
+        $where_condition = 'WHERE user_id = '.$this->user->user_id.' AND user_id_followed = '.$user_id_followed;
+            
+        # Run the delete
+        DB::instance(DB_NAME)->delete('users_users', $where_condition);
+        
+        # Send them back
+        Router::redirect("/posts/users");
+        
+        }
 
 } # end of the class
