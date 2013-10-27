5e6b6db45fe89dd031b076e90b46df30a3794342
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-26 21:14:54 -0400

    Changed order of posts to be sorted by date/time

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -32,7 +32,10 @@ class posts_controller extends base_controller {
                     ON posts.user_id = users_users.user_id_followed
                 INNER JOIN users 
                     ON posts.user_id = users.user_id
-                WHERE users_users.user_id = '.$this->user->user_id;
+                WHERE users_users.user_id = '.$this->user->user_id.'
+                ORDER BY posts.created';
+
+        echo $sql;
 
         $posts = DB::instance(DB_NAME)->select_rows($sql);
 
