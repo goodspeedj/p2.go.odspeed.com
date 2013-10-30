a2ae2805a1c0485b6993b9c63c14bd38ca442c05
Jim Goodspeed <jgoodsp@fas.harvard.edu>
2013-10-29 21:16:43 -0400

    Fixed nav bar highlighting

--- views/_v_navbar.php
+++ views/_v_navbar.php
@@ -6,26 +6,45 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-
-      <?=$_SERVER['REQUEST_URI'];?>
       
       <div class="offset2">
         <a class="brand" href="/users/login">Flitter</a>
         
         <div class="nav-collapse collapse offset1">
           <ul class="nav">
+
+          <?php if($_SERVER['REQUEST_URI'] == "/index/index"): ?>
             <li class="active">
+          <?php else: ?>
+            <li>
+          <?php endif; ?>
               <a href="/index/index">Home</a>
             </li>
+
+          <?php if($_SERVER['REQUEST_URI'] == "/posts/index"): ?>
+            <li class="active">
+          <?php else: ?>
             <li>
+          <?php endif; ?>
               <a href="/posts/index">Post</a>
             </li>
+
+          <?php if($_SERVER['REQUEST_URI'] == "/posts/users"): ?>
+            <li class="active">
+          <?php else: ?>
             <li>
+          <?php endif; ?>
               <a href="/posts/users">Users</a>
             </li>
+
+          <?php if($_SERVER['REQUEST_URI'] == "/users/profile"): ?>
+            <li class="active">
+          <?php else: ?>
             <li>
+          <?php endif; ?>
               <a href="/users/profile">Profile</a>
             </li>
+            
             <li>
               <a href="/users/logout">Logout</a>
             </li>
